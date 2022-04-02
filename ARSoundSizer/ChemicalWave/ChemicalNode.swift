de3bb0b951247806553c8393ccdb7007662d7066
Spencer <37875712+Penjat@users.noreply.github.com>
2022-04-02 07:28:15 -0700

    Working.

--- ARSoundSizer/ChemicalWave/ChemicalNode.swift
+++ ARSoundSizer/ChemicalWave/ChemicalNode.swift
@@ -1,8 +1,14 @@
 import Foundation
 
 class ChemicalNode {
-    var position: (x: Double, y: Double, z: Double)
-    init(position: (x: Double, y: Double, z: Double)) {
+    var position: (x: Int, y: Int, z: Int)
+    init(position: (x: Int, y: Int, z: Int)) {
         self.position = position
     }
+    var state = NodeState.idle
+}
+
+enum NodeState {
+    case idle
+    case charging
 }
--- ARSoundSizer/ChemicalWave/ChemicalWaveViewModel.swift
+++ ARSoundSizer/ChemicalWave/ChemicalWaveViewModel.swift
@@ -12,7 +12,7 @@ class ChemicalWaveViewModel: ObservableObject {
     
     var arView: ARView?
     var gridAnchor: AnchorEntity?
-    
+    var entities = [Entity: ChemicalNode]()
     
     var gridSpacing: Float {
         return Constants.sphereSize*2 + Constants.gridSpace
@@ -25,43 +25,33 @@ class ChemicalWaveViewModel: ObservableObject {
     
     func buildScene() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(rec:)))
-
-        //Add recognizer to sceneview
         arView?.addGestureRecognizer(tap)
         arView?.environment.background = .color(.blue)
         gridAnchor = AnchorEntity(world: [0,-1,-0.5])
         arView?.scene.addAnchor(gridAnchor!)
+        let node = try! ARAssets.loadBox().chemicalNode
+        print(node?.availableAnimations)
         for row in 0..<Constants.numRows {
             for col in 0..<Constants.numCols {
-                let boxMesh = MeshResource.generateSphere(radius: Constants.sphereSize)
-                let boxMaterial = SimpleMaterial(color: .blue, roughness: 0.2, isMetallic: true)
+                //                let boxMesh = MeshResource.generateSphere(radius: Constants.sphereSize)
+                //                let boxMaterial = SimpleMaterial(color: .blue, roughness: 0.2, isMetallic: true)
                 
                 let x: Float = Float(row)*gridSpacing
                 let y: Float = Float(0)
                 let z: Float = Float(col)*(-1)*gridSpacing
                 
-                let boxEntity = ModelEntity(mesh: boxMesh, materials: [boxMaterial])
-    //            entities.append((boxEntity, gridElement))
+                let boxEntity = node!.clone(recursive: true)//ModelEntity(mesh: boxMesh, materials: [boxMaterial])
+                let node = ChemicalNode(position: (x: row, y: 0, z: col))
+                entities[boxEntity] = node
+                
                 boxEntity.position = [x,y,z]
-                boxEntity.collision = CollisionComponent(
-                    shapes: [.generateSphere(radius: Constants.sphereSize)],
-                    mode: .trigger,
-                    filter: .sensor
-                )
                 
                 gridAnchor?.addChild(boxEntity)
             }
         }
-
     }
     
-   
-
-    //Method called when tap
     @objc func handleTap(rec: UITapGestureRecognizer){
-        
-        
-        
         let location = rec.location(in: arView!)
         print(location)
         guard let entity = arView!.entities(at: location).first else {
@@ -69,6 +59,43 @@ class ChemicalWaveViewModel: ObservableObject {
             return
         }
         
-        entity.removeFromParent()
+        fireNode(entity: entity)
+    }
+    
+    func fireNode(entity: Entity) {
+        guard let node = entities[entity] else {
+            return
+        }
+        node.state = .charging
+        let rotation = Transform(scale: SIMD3<Float>(2.0, 2.0, 2.0), translation: entity.position)
+        
+        entity.move(to: rotation,
+                    relativeTo: gridAnchor,
+                    duration: 0.5,
+                    timingFunction: .easeInOut)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            
+                entity.move(to: Transform(scale: SIMD3<Float>(1.0, 1.0, 1.0), translation: entity.position),
+                            relativeTo: self.gridAnchor,
+                        duration: 0.5,
+                        timingFunction: .easeInOut)
+        }
+        
+        let neighbors = entities.filter { (_, value) in
+            abs(value.position.x - node.position.x) <= 1 && abs(value.position.y - node.position.y) <= 1 && abs(value.position.z - node.position.z) <= 1
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+            neighbors.filter{ $0.value.state == .idle }.forEach { (entity, _) in
+                self.fireNode(entity: entity)
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            neighbors.forEach { (_, node) in
+                node.state = .idle
+            }
+        }
     }
 }
